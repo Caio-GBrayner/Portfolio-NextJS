726b02427a8e715cfc1f78106a2c76556abc5a86
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-07-12 15:04:53 -0300

    feat(function) ActiveItem:
    1. NavBar
    2. ResponsiveNavBar

--- portfolio-next/src/components/NavBar/NavBar.tsx
+++ portfolio-next/src/components/NavBar/NavBar.tsx
@@ -1,25 +1,44 @@
+'use client';
+
 import React from 'react'
 import LogoNav from './LogoNav'
 import { navLinks } from '@/constant/constant'
 import Link from 'next/link'
 
-const NavBar = () => {
+type NavBarProps = {
+  activeItem: string;
+  setActiveItem: (id: string) => void;
+};
+
+const NavBar = ({ activeItem, setActiveItem }: NavBarProps) => {
   return (
     <div className='flex items-center justify-between h-24'>
       <LogoNav/>
       <div className='hidden md:flex items-center gap-3'>
         {navLinks.map((link) => {
+          const linkClasses =`
+            relative px-8 py-4 text-base font-semibold rounded-xl
+            transition-all duration-300
+            ${activeItem === link.id
+              ? 'text-primary bg-primary/10 border border-primary/20' 
+              : 'text-muted-foreground hover:text-foreground hover:bg-secondary/50'
+            }
+          `;
           return (
-            <Link className='relative px-8 py-4 text-base font-semibold rounded-xl transition-all duration-300 text-muted-foreground hover:text-foreground hover:bg-secondary/50'
+            <Link className={linkClasses}
              key={link.id} href={link.url}
+             onClick={() => setActiveItem(link.id)}
              >
-              {link.label}
+              <div className='flex items-center gap-2'>
+                {link.label}
+              </div>
+              
             </Link>
           )
         })}
       </div>
       <div className='hidden md:flex items-center gap-4'>
-        <button className='bg-background relative w-10 h-10 rounded-full'>
+        <button className='relative w-10 h-10 rounded-full bg-white/10 dark:bg-white/10 backdrop-blur-md border border-white/20 dark:border-white/20 hover:bg-white/20 dark:hover:bg-white/20 transition-all duration-300 flex items-center justify-center'>
           teste
         </button>
         <Link href={"#"} className='px-6 py-3 rounded-lg bg-primary hover:bg-primary/90 text-primary-foreground font-semibold transition-all duration-300 hover:shadow-lg hover:shadow-primary/25 text-base'>
--- portfolio-next/src/components/NavBar/ResponsiveNavBar.tsx
+++ portfolio-next/src/components/NavBar/ResponsiveNavBar.tsx
@@ -1,14 +1,53 @@
-import React from 'react'
-import NavBar from './NavBar'
+'use client'; 
 
-const ResponsiveNavBar = () => {
+import React, { useEffect, useState } from 'react'
+import NavBar from './NavBar'
+import { usePathname } from 'next/navigation';
+import { navLinks } from '@/constant/constant'
+import MobileNavBar from './MobileNavBar'
+
+export default function ResponsiveNavBar() {
+  const pathname = usePathname();
+  const [activeItem, setActiveItem] = useState('');
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect (() => {
+    const foundItem = navLinks.find(link => link.url === pathname);
+    if (foundItem) {
+      setActiveItem(foundItem.id);
+    }else {
+      setActiveItem('1');
+    }
+  }, [pathname]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+
+    window.removeEventListener('resize', handleResize);
+
+    return () => window.addEventListener('resize', handleResize);
+  }, []);
+
   return (
     <nav className='fixed top-0 left-0 right-0 z-50 bg-background/95 backdrop-blur-xl border-b-4 border-border shadow-lg'>
       <div className='max-w-8xl mx-auto px-6  sm:px-8 lg:px-12'>
-        <NavBar/>
+        {isMobile ? (
+          <MobileNavBar
+            activeItem={activeItem}
+            setActiveItem={setActiveItem}
+          />
+        )
+        : (
+          <NavBar
+            activeItem={activeItem}
+            setActiveItem={setActiveItem}
+          />
+        )}
       </div>
     </nav>
-  )
+  );
 }
-
-export default ResponsiveNavBar
