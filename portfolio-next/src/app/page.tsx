0f23c6b183100e02d3e53776737eb6da18fc7d60
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-07-14 19:41:26 -0300

    feat(Home) Home:
    1. Added Footer
    2. Added Approach Section
    3. Added Testimonials Section

--- portfolio-next/src/app/page.tsx
+++ portfolio-next/src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import Footer from "@/components/footer/Footer";
+import ApproachSection from '@/components/section/ApproachSection'
+import TestimonialsSection from '@/components/section/TestimonialsSection'
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center flex-col mx-auto">
+      <TestimonialsSection/>
+      <ApproachSection/>
       <Footer/>
     </main>
   );
