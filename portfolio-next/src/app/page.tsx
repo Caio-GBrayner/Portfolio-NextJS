0e51a55f07fb1f2d83558d34cf4c521671bb5329
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-07-16 14:39:55 -0300

    feat(Page) Home:
    1. Added BackgrounDecor
    2. Added HeroContent

--- portfolio-next/src/app/page.tsx
+++ portfolio-next/src/app/page.tsx
@@ -2,10 +2,21 @@ import Image from "next/image";
 import Footer from "@/components/footer/Footer";
 import ApproachSection from '@/components/section/ApproachSection'
 import TestimonialsSection from '@/components/section/TestimonialsSection'
+import GridExample from "@/components/about/GridExample";
+import AboutGridSection from "@/components/about/AboutGridSection";
+import BackgroundDecor from "@/components/hero/BackgroundDecor";
+import HeroContent from "@/components/hero/HeroContent";
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center flex-col mx-auto">
+      <div id="hi" className="w-full min-h-screen overflow-hidden -mt-24 md:mt-0">
+      <div className="relative min-h-screen flex flex-col items-center justify-center bg-background dark:bg-black overflow-hidden">
+        <BackgroundDecor />
+        <HeroContent />
+      </div>
+    </div>
+      <AboutGridSection/>
       <TestimonialsSection/>
       <ApproachSection/>
       <Footer/>
