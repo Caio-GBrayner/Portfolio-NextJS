a79335b6b824c31eea70fd38da179ed3f61ba084
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-07-24 19:09:42 -0300

    #

--- portfolio-next/src/app/page.tsx
+++ portfolio-next/src/app/page.tsx
@@ -10,12 +10,12 @@ import HeroContent from "@/components/hero/HeroContent";
 export default function Home() {
   return (
     <main className="flex justify-center items-center flex-col mx-auto">
-      <div id="hi" className="w-full min-h-screen overflow-hidden -mt-24 md:mt-0">
-      <div className="relative min-h-screen flex flex-col items-center justify-center bg-background dark:bg-black overflow-hidden">
-        <BackgroundDecor />
-        <HeroContent />
+     <div id="hi" className="w-full mt-24 md:mt-0">
+        <div className="relative min-h-screen flex flex-col items-center justify-center bg-background dark:bg-black overflow-hidden">
+          <BackgroundDecor />
+          <HeroContent />
+        </div>
       </div>
-    </div>
       <AboutGridSection/>
       <TestimonialsSection/>
       <ApproachSection/>
